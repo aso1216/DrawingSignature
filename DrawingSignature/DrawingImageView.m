fac0ce2f6f9889c6ac9affdcde6d655682ef96f8
Toren <toren@realtime.tw>
2018-02-05 14:45:20 +0800

    [Drawing]套用touch繪圖路徑

--- DrawingSignature/DrawingImageView.m
+++ DrawingSignature/DrawingImageView.m
@@ -10,7 +10,12 @@
 
 @interface DrawingImageView ()
 {
-
+    UIBezierPath *mLinePath; //繪圖貝茲曲線
+    CAShapeLayer *mLineShapeLayer; //繪圖Layer
+    CGPoint lastPoint; //最後移動位置
+    BOOL isMoving; //是否正在移動中
+    float lineWidth; //繪圖筆寬度
+    UIColor *lineColor; //繪圖筆顏色
 }
 @end
 
@@ -18,18 +23,48 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    [self setUserInteractionEnabled:YES];
+    [self defaultInit];
 }
 
--(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+- (void)defaultInit{
+    [self setUserInteractionEnabled:YES];
+    isMoving = NO;
+    lineWidth = 4;
+    lineColor = [UIColor redColor];
+    mLinePath = [[UIBezierPath alloc] init];
+    mLineShapeLayer = [[CAShapeLayer alloc] init];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [super touchesEnded:touches withEvent:event];
     NSLog(@"touchesEnded");
-    
+    isMoving = NO;
 }
 
--(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [super touchesMoved:touches withEvent:event];
     NSLog(@"touchesMoved");
+    CGPoint movePen = [[touches anyObject] locationInView:self];
+    if (movePen.x <0 || movePen.y <0) {
+        return;
+    }
+    
+    if (movePen.x > self.frame.size.width || movePen.y > self.frame.size.height) {
+        return;
+    }
+
+    if (!isMoving) {
+        isMoving = YES;
+        [mLinePath moveToPoint:movePen];
+    }else{
+        [mLinePath addLineToPoint:movePen];
+        mLineShapeLayer.lineWidth = lineWidth;
+        mLineShapeLayer.strokeColor = lineColor.CGColor;
+        mLineShapeLayer.path = mLinePath.CGPath;
+        mLineShapeLayer.fillColor = nil;
+    }
+    
+    [self.layer addSublayer:mLineShapeLayer];
 }
 
 @end
